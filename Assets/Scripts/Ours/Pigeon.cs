0c84b482c0c91a7e3d6212ee3d6174068e9d10ee
GregorFC <g.e.figueiracomojo@student.tudelft.nl>
2023-04-29 18:13:32 +0200

    Basic pigeon layer switching

--- Assets/Scripts/Ours/Pigeon.cs
+++ Assets/Scripts/Ours/Pigeon.cs
@@ -9,10 +9,19 @@ public class Pigeon : MonoBehaviour {
     // Flap force
     public float force = 300;
 
+    // Current collision layer
+    public int collisionLayer = 0;
+
+    private int upperBound = 0;
+
+    public GameObject grid;
+
     // Use this for initialization
     void Start() {
         // Fly towards the right
         GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+        upperBound = grid.GetComponent<Collision>().layers.Length;
+        
     }
 
     // Update is called once per frame
@@ -20,5 +29,13 @@ public class Pigeon : MonoBehaviour {
         // Flap
         if (Input.GetKeyDown(KeyCode.Space))
             GetComponent<Rigidbody2D>().AddForce(Vector2.up * force);
+
+        // Move to a layer up
+        if (Input.GetKeyDown(KeyCode.W) && collisionLayer + 1 < upperBound)
+            collisionLayer++;
+
+        // Move to a layer down 
+        if (Input.GetKeyDown(KeyCode.S) && collisionLayer > 0)
+            collisionLayer--;
     }
 }
