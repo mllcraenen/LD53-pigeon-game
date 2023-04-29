d89430c0f6ffbc8a30cce039be3cf083ac50cc8a
Marijn Craenen <marijn@craenen.tech>
2023-04-29 18:11:50 +0200

    Pigeon basic rotation

--- Assets/Scripts/Ours/Pigeon.cs
+++ Assets/Scripts/Ours/Pigeon.cs
@@ -1,24 +1,38 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using UnityEditor;
 
 public class Pigeon : MonoBehaviour {
     // Movement speed
     public float speed = 2;
 
+    Vector3 mPos;
+
     // Flap force
     public float force = 300;
+    public float rotationSpeed = 5f;
 
-    // Use this for initialization
+
     void Start() {
         // Fly towards the right
         GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
-    }
+	}
 
-    // Update is called once per frame
     void Update() {
         // Flap
-        if (Input.GetKeyDown(KeyCode.Space))
-            GetComponent<Rigidbody2D>().AddForce(Vector2.up * force);
+        if (Input.GetMouseButtonDown(0))
+			GetComponent<Rigidbody2D>().AddForce(transform.up * force);
+
+        mPos = Input.mousePosition;
+        transform.rotation = Quaternion.Euler(0, 0, Mathf.Lerp(-90, 90, Mathf.InverseLerp(0, Screen.height, mPos.y)));
     }
+    //void OnDrawGizmos() {
+    //    Debug.DrawRay(transform.position, transform.up, Color.yellow);
+    //    GUIStyle biggus = new GUIStyle();
+    //    biggus.fontSize = 30;
+    //    biggus.normal.textColor = Color.white;
+    //    Gizmos.color = Color.white;
+    //    Handles.Label(transform.position, mPos.ToString(), biggus);
+    //}
 }
